d1526c56b478019f92fd65f871edc83c38cff007
Yeikeit <ju.tolorza@duocuc.cl>
2025-05-16 20:00:40 -0400

    add service methods

--- src/main/java/com/retailmax/ordenes/services/OrderService.java
+++ src/main/java/com/retailmax/ordenes/services/OrderService.java
@@ -1,5 +1,26 @@
 package com.retailmax.ordenes.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.retailmax.ordenes.repository.OrderRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    
+    public void getOrders(){}
+    public void addOrder(){}
+    public void getOrderById(String id){}
+    public void updateOrder(String id){}
+    public void deleteOrder(String id){}
+
 
 }
