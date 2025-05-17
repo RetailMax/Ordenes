a1ef11dc8e10d2c4a20ccceee07072905d8915a6
Luisafernanda2005 <luis.espinozap@duocuc.cl>
2025-05-16 22:14:37 -0400

    update aplication properties

--- src/main/java/com/retailmax/ordenes/model/Order.java
+++ src/main/java/com/retailmax/ordenes/model/Order.java
@@ -2,17 +2,35 @@ package com.retailmax.ordenes.model;
 
 import java.util.Date;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+//@Entity
+//@Table(name = "order")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
+
+    //@Column(nullable = false)
     private String userId;
+
+    //@Column(nullable = false)
     private Date orderDate;
+
+   
     private Double totalAmount;
     private Double shippingFee;
     private Integer statusId;
