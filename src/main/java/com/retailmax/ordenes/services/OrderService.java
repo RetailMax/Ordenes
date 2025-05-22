0da32caa015122833c7fdc82bf730cc46b35ce48
Yeikeit <ju.tolorza@duocuc.cl>
2025-05-22 01:24:28 -0400

    changes for a successfull db connection

--- src/main/java/com/retailmax/ordenes/services/OrderService.java
+++ src/main/java/com/retailmax/ordenes/services/OrderService.java
@@ -21,20 +21,20 @@ public class OrderService {
         return orderRepository.findAll();
     }
 
-    public Boolean addOrder(Order order) {
-        return orderRepository.addNewOrder(order);
+    public Order addOrder(Order order) {
+        return orderRepository.save(order);
     }
 
-    public Order getOrderById(String id) {
-        return orderRepository.findById(id);
+    public Order getOrderById(Long id) {
+        return orderRepository.findById(id).get();
     }
 
-    public Boolean updateOrder(Order order) {
-        return orderRepository.updateOrder(order);
-    }
+    //public Boolean updateOrder(Order order) {
+      //  return orderRepository.updateOrder(order);
+    //}
 
-    public Boolean deleteOrder(String id) {
-        return orderRepository.deleteOrderById(id);
+    public void delete(Long id){
+        orderRepository.deleteById(id);
     }
 
 }
